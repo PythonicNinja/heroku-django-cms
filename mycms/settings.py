d6d6f640dc0ac202b5df9de7aea0deda32639c76
Wojtek Nowak <vojtek.nowak@gmail.com>
2013-07-09 12:40:59 +0200

    commitment

--- mycms/settings.py
+++ mycms/settings.py
@@ -18,7 +18,8 @@ LANGUAGES = [('en', 'en')]
 DEFAULT_LANGUAGE = 0
 
 # Heroku will take care of this for us later dynamicly.
-DATABASES = {}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
